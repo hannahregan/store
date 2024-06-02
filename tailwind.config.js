72fcae28105376ed6f408216166a9514961b6511
Hannah Regan <hannhregan11@gmail.com>
2024-06-02 23:40:27 +1200

    adding tailwind things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}", // Adjust this according to your project structure
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
