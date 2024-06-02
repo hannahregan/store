36a4d0f3d570940b9054bbd8619fc9d9701ae021
Hannah Regan <hannhregan11@gmail.com>
2024-06-03 00:58:00 +1200

    added a dummy image and starting building product box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 import { FiltersComponent } from './pages/home/components/filters/filters.component';
+import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 
 
 @NgModule({
@@ -35,6 +36,7 @@ import { FiltersComponent } from './pages/home/components/filters/filters.compon
     HomeComponent,
     ProductsHeaderComponent,
     FiltersComponent,
+    ProductBoxComponent,
 
   ],
   imports: [
