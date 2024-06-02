66ea47329450e85eaa86bbd7c6bf6326a38c64db
Hannah Regan <hannhregan11@gmail.com>
2024-06-03 00:39:53 +1200

    adding categories and fixing shopping cart

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filters.component.css']
 })
 export class FiltersComponent implements OnInit {
+  @Output() showCategory = new EventEmitter<string>();
+  categories = ['Bad Ross', 'Ghibli Goodies', 'Others'];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+
   }
 
 }
